e05d7e1283dacf83c425e72aa21ec73591ef7bf4
benzy78 <westvillage815@gmail.com>
2025-01-12 22:38:44 +0900

    レスポンシブ対応

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-$(function(){
-    
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".header_nav");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
 });
+
